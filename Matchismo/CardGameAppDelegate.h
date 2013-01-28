6661112147e04ba02db42d2cf45b1a5baaa4a14e
Michael Meinberg <fogcitysolutions@comcast.net>
2013-01-28 11:48:46 -0800

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Fog City Solutions on 1/28/13.
+//  Copyright (c) 2013 Fog City Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Fog City Solutions on 1/28/13.
+//  Copyright (c) 2013 Fog City Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
